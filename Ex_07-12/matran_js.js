47cb0c20a2d6a273f33be8faef64c0f9db8ca05d
uyensiuxinhhh <93874033+uyensiuxinhhh@users.noreply.github.com>
2021-12-08 07:39:10 +0700

    Add files via upload

---
+++ Ex_07-12/matran_js.js
@@ -0,0 +1,15 @@
+// function display() {
+//     for ( let i = 0; i < 5; i++) {
+//         for ( let j = 0; j < 20; j++) {
+//             row += "<tr>" + "</tr>"
+//             document.document.write("<div class='square'></div>");
+//         }
+//     }
+// }
+
+for (var i = 0; i < 5; i++) {
+    for (var j = 0; j < 10; j++) {
+        document.write("<div class='square'></div>");
+    }
+    document.write("<div style='clear:both'></div>");
+}
